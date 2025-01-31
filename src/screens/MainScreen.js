7aad8157e799317d487f876320dc5b22f448cf72
Ali-El-Helbawi <ali.al.helbawi.2017@gmail.com>
2025-01-31 14:04:43 +0200

    add VoicebotScreen in main screen as Modal

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,11 +1,11 @@
 import {CommonActions, useNavigation} from '@react-navigation/native';
 import React, {useEffect} from 'react';
-import {View, Text, Pressable, StyleSheet, Image} from 'react-native';
+import {View, Text, Pressable, StyleSheet, Image, Modal} from 'react-native';
 const MainScreen = props => {
   const navigation = useNavigation();
   const resetStackVal = props?.route?.params?.resetStack || false;
   const [resetStack, setResetStack] = React.useState(resetStackVal);
-
+  const [showModal, setShowModal] = React.useState(false);
   useEffect(() => {
     if (resetStack) {
       navigation.dispatch(
@@ -20,21 +20,59 @@ const MainScreen = props => {
 
   return (
     <View style={Styles.container}>
-      <Pressable onPress={() => navigation.navigate('SettingStack')}>
+      <Modal
+        animationType="slide"
+        navigationBarTranslucent={true}
+        statusBarTranslucent={true}
+        transparent={false}
+        visible={showModal}
+        onRequestClose={() => {
+          setShowModal(!showModal);
+        }}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Pressable
+            onPress={() => {
+              setShowModal(false);
+            }}
+            style={{position: 'absolute', top: 50, right: 0}}>
+            <Image
+              source={require('../images/close_icon.png')}
+              style={{width: 60, height: 60}}
+              resizeMode="contain"
+            />
+          </Pressable>
+          <Text style={[Styles.text, {color: '#000'}]}>Voice bot screen</Text>
+        </View>
+      </Modal>
+      <Pressable
+        onPress={() => navigation.navigate('SettingStack')}
+        style={{position: 'absolute', top: 0, left: 0}}>
         <Image
           source={require('../images/settings_icon.png')}
           style={{width: 60, height: 60}}
           resizeMode="contain"
         />
       </Pressable>
+      <View style={Styles.subContainer}>
+        <Pressable
+          style={Styles.button}
+          onPress={() => {
+            setShowModal(true);
+          }}>
+          <Text style={Styles.text}>Launch Voicebot screen</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
+  },
+  subContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
     backgroundColor: 'blue',
